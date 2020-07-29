29e6f2437a25114545041088e3a98be723d1a863
Gemma Standley <gjs97@cornell.edu>
2020-07-29 12:14:10 -0400

    Updates

---
+++ precache-manifest.e369c69279cb3046145e70426a1d658b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4809dccd8863d38c839fcfb5be0da701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b910004628af5282e4",
+    "url": "/static/css/main.3568dad6.chunk.css"
+  },
+  {
+    "revision": "d02710cbec531bed6cfe",
+    "url": "/static/js/2.9f6c2c38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9f6c2c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b910004628af5282e4",
+    "url": "/static/js/main.8dec5f33.chunk.js"
+  },
+  {
+    "revision": "16c0566fead19b1e7639",
+    "url": "/static/js/runtime-main.39c019fa.js"
+  },
+  {
+    "revision": "ac98dd453f7cc5a17a279ccdcee6c21d",
+    "url": "/static/media/courseSourceSquareRec.ac98dd45.mov"
+  },
+  {
+    "revision": "81599e08fb7cfb9e6175e876fe4bfac5",
+    "url": "/static/media/doodleRec.81599e08.mov"
+  },
+  {
+    "revision": "045703a49dba2ff09128e4993daf84db",
+    "url": "/static/media/gemma_photo.045703a4.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4fcf8e56d9b6cee2d37f3ef24ee1f4d0",
+    "url": "/static/media/oSnakeSquareRec.4fcf8e56.mov"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47c90ceb2ae00ca965348a9c656af352.js"
+  "/precache-manifest.e369c69279cb3046145e70426a1d658b.js"
 );
 
 self.addEventListener('message', (event) => {
